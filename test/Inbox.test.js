e21d7af1ca957127fdae2d75ac13130a50b4ac34
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-08-21 21:47:17 +0800

    fetch accont from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,26 +5,20 @@ const Web3 = require("web3"); //huruf besart karena web3 adalah fungsi construct
 
 const web3 = new Web3(ganache.provider()); //argumen proovider dapat diganti
 
-class Car {
-   park() {
-     return "stopped";
-   }
-   drive() {
-     return "vroom";
-   }
- }
- 
- let car;
- 
- beforeEach(() => {
-   car = new Car();
- });
- 
- describe("Car", () => { //argumen pertman hnaya untuk describe jadi tidak terlalu penting
-   it("can park and return stopped", () => {
-     assert.equal(car.park(), "stopped");
-   });
-   it("can drive", () => {
-     assert.equal(car.drive(), "vroom");
-   });
- });
+let accounts;
+
+beforeEach(async () => {
+  //get a list of alll accounts
+  try {
+    accounts = await web3.eth.getAccounts();
+  } catch (error) {}
+
+  //use one of those accounts to deploy the contract
+  //   inbox = new web3.eth.Contract(JSON.parse(compiledInbox.interface));
+});
+
+describe("Inbox", () => {
+  it("deploy a contract", () => {
+    console.log(accounts);
+  });
+});
