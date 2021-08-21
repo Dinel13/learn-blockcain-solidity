ae6d54dbc2e6cf1546d973473df53459e3c71d2d
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-08-21 21:55:54 +0800

    deploy with web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,10 +2,12 @@
 const assert = require("assert"); //assert library
 const ganache = require("ganache-cli");
 const Web3 = require("web3"); //huruf besart karena web3 adalah fungsi constructor
+const { interface, bytecode } = require("../compile.js");
 
 const web3 = new Web3(ganache.provider()); //argumen proovider dapat diganti
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   //get a list of alll accounts
@@ -14,11 +16,13 @@ beforeEach(async () => {
   } catch (error) {}
 
   //use one of those accounts to deploy the contract
-  //   inbox = new web3.eth.Contract(JSON.parse(compiledInbox.interface));
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["HI there"] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploy a contract", () => {
-    console.log(accounts);
+    console.log(inbox);
   });
 });
